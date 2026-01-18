f812466b77e66ae393628db377db45e599540d7e
harish-kumaran-m <harishkumaran1003@gmail.com>
2026-01-18 14:38:36 +0530

    Create MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,24 @@
+package demo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class MainApp {
+    static Connection connect() {
+        try {
+        	System.out.println("Database Connected");
+        	Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/school_db", "root", "");
+        	return conn;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        Log l1=new Log();
+        l1.setVisible(true);
+        
+    }
+}
